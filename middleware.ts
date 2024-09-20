508a63de798d4a2aad5fba27a009ae56e54179a0
Julien Goux <hi@jgoux.dev>
2024-09-20 12:34:58 +0200

    format

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,18 @@
-import { supabaseMiddleware, createRouteMatcher } from '@supabase-labs/nextjs/server'
+import {
+  supabaseMiddleware,
+  createRouteMatcher
+} from '@supabase-labs/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/login(.*)', '/signup(.*)'])
 
 export default supabaseMiddleware(
   (auth, request) => {
     if (!isPublicRoute(request)) {
-      auth().protect()
+      auth().protect(has => has({ user_metadata: { roles: ['admin'] } }))
     }
 
     if (isPublicRoute(request) && auth().user) {
-      auth().redirect("/")
+      auth().redirect('/')
     }
   },
   {
